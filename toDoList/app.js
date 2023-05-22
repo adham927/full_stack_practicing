d4623c6f3d810a7852b0ebf50c63768c43ed330c
Adham <ad@gmail.com>
2023-05-22 22:17:37 +0300

    using mongodb

--- toDoList/app.js
+++ toDoList/app.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
 const todoitems = ["go to work","take a break","sleep"];
@@ -7,6 +8,18 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
+const mongoDB = "mongodb://127.0.0.1/toDoListDB";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+const Schema = mongoose.Schema;
+const WorkItemss = new Schema({
+    name: String
+  });
+const workItem = mongoose.model("wItem", WorkItemss);
 
 app.get("/",function(req,res){
 var day;
@@ -15,15 +28,25 @@ var today  = new Date();
 day = today.toLocaleDateString("en-US", options);
 res.render("list", {kindOfDay:day, itemarrays:todoitems});
 })
+async function getItems(){
 
+    const Items = await workItem.find({});
+    return Items;
+  
+  }
 app.get("/work", function(req,res){
-    res.render("list", {kindOfDay:"Work List", itemarrays:workItems});
+    getItems().then(function(result){
+    
+        res.render("list", {kindOfDay:"Work List", itemarrays: result});    
+      });
 })
 
 app.post("/",function(req,res){
     if(req.body.button == "Work List"){
         const userinput = req.body.sometext;
-        workItems.push(userinput);
+        // workItems.push(userinput);
+        const newItem = new workItem({ name: userinput});
+        newItem.save();
         res.redirect("/work");
     }
     else{

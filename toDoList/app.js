a243330a5890ff9db3c5253b175dcf13b51e1418
Adham <ad@gmail.com>
2023-05-22 20:58:03 +0300

    improve the code

--- toDoList/app.js
+++ toDoList/app.js
@@ -1,22 +1,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const todoitems = ["go to work","take a break","sleep"];
+const workItems = [];
 var app = express();
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/",function(req,res){
-var today = new Date();
 var day;
-if(today.getDay()<=4){
-    day = "weekday";
-}
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var today  = new Date();
+day = today.toLocaleDateString("en-US", options);
+res.render("list", {kindOfDay:day, itemarrays:todoitems});
+})
 
-else{
-    day = "weekend";
-}
-res.render("list", {kindOfDay:day});
+app.get("/work", function(req,res){
+    res.render("list", {kindOfDay:"Work List", itemarrays:workItems});
+})
+
+app.post("/",function(req,res){
+    if(req.body.button == "Work List"){
+        const userinput = req.body.sometext;
+        workItems.push(userinput);
+        res.redirect("/work");
+    }
+    else{
+        const userinput = req.body.sometext;
+        todoitems.push(userinput);
+        res.redirect("/");
+    }
+    
 })
 
 app.listen("3000",function(){

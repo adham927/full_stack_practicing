c12f5ff605f00dda7e7be25e0d33501b9b4e79be
Adham <ad@gmail.com>
2023-05-31 17:23:43 +0300

    adding some functionality to the code

--- toDoList/app.js
+++ toDoList/app.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
-const todoitems = ["go to work","take a break","sleep"];
+const _ = require("lodash");
+const todoitems = [];
 const workItems = [];
 var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
-const mongoDB = "mongodb://127.0.0.1/toDoListDB";
+const mongoDB = "mongodb+srv://ameradham152:ameradham@shopdbcluster.4fw6m0v.mongodb.net/toDoListDB";
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -16,45 +17,138 @@ async function main() {
 }
 
 const Schema = mongoose.Schema;
-const WorkItemss = new Schema({
-    name: String
-  });
-const workItem = mongoose.model("wItem", WorkItemss);
+// const WorkItemss = new Schema({
+//     name: String
+//   });
+// const workItem = mongoose.model("wItem", WorkItemss);
+const dayTodayItems = new Schema({
+  name: String
+});
+const dayTodayItemss = mongoose.model("dItem", dayTodayItems);
+
+const lists = new Schema({
+  name: String,
+  items: []
+});
+const listss = mongoose.model("list", lists);
 
 app.get("/",function(req,res){
-var day;
-var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-var today  = new Date();
-day = today.toLocaleDateString("en-US", options);
-res.render("list", {kindOfDay:day, itemarrays:todoitems});
+// var day;
+// var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+// var today  = new Date();
+// day = today.toLocaleDateString("en-US", options);
+getItems().then(function(result){
+  res.render("list", {kindOfDay:"Today", itemarrays: result});
 })
-async function getItems(){
+})
+//  async function getWorkItems(){
 
-    const Items = await workItem.find({});
+//     const Items = await workItem.find({});
+//     return Items;
+  
+//   }
+  async function getItems(){
+
+    const Items = await dayTodayItemss.find({});
     return Items;
   
   }
-app.get("/work", function(req,res){
-    getItems().then(function(result){
+// app.get("/work", function(req,res){
+
+
+
+//     getWorkItems().then(function(result){
     
-        res.render("list", {kindOfDay:"Work List", itemarrays: result});    
-      });
-})
+//         res.render("list", {kindOfDay:"Work List", itemarrays: result});    
+//       });
+// })
 
 app.post("/",function(req,res){
-    if(req.body.button == "Work List"){
-        const userinput = req.body.sometext;
-        // workItems.push(userinput);
-        const newItem = new workItem({ name: userinput});
-        newItem.save();
-        res.redirect("/work");
+  const userinput = req.body.sometext;
+  const listTitle = req.body.button;
+
+  const item = new dayTodayItemss({
+    name: userinput
+  })
+
+  if(listTitle=="Today"){
+        item.save();
+        res.redirect("/");
+  }
+  else{
+    listss.findOne({name: listTitle}).then((foundone)=>{
+    if(foundone){
+      foundone.items.push(item);
+      foundone.save();
+      res.redirect("/" + listTitle);
+    }
+     
+    })
+  }
+})
+
+app.post("/delete", function(req,res){
+//   const listName = req.body.listName;
+//   const itemId = req.body.input;
+//   console.log(itemId);
+
+//   if(listName == "Today"){
+//     await dayTodayItemss.findByIdAndRemove(req.body.input)
+//     .then(()=>console.log(`Deleted Successfully`)).catch((err) => console.log("Deletion Error: " + err));
+//     res.redirect("/");
+//   }
+//   else{
+//     //  listss.findOneAndUpdate( {name: listName}, {$pull: { items: { _id: itemId } } }, {new:true})
+//     // .then(()=>{console.log(`Item Deleted Successfully`);
+//     //  res.redirect("/"+listName);})
+//     //  .catch((err) => console.log("there is Deletion Error: " + err));
+    
+//     await listss.findOneAndUpdate({name:listName}, {$pull: {items: {_id: itemId}}}, {
+//       new: true
+//     }).then(function (foundList)
+//     {
+//       res.redirect("/" + listName);
+//     }).catch( err => console.log(err));
+// }
+const checkedItem = req.body.input;
+const listName = req.body.listName;
+
+if(listName === "today"){
+ dayTodayItemss.deleteOne({_id: checkedItem}).then(function () {
+     console.log("Successfully deleted");
+     res.redirect("/");
+  })
+  .catch(function (err) {
+     console.log(err);
+   });
+}else{
+  listss.findOneAndUpdate({name: listName}, {$pull: {items: {name: checkedItem}}}, {new: true}).then(function (foundList)
+  {
+    res.redirect("/" + listName);
+  });
+}
+});
+
+app.get("/:customList",function(req,res){
+   const customList = _.capitalize(req.params.customList);
+
+ 
+  listss.findOne({name: customList}).then((foundone)=>{
+    if(!foundone){
+      const newList = new listss({ name: customList, items: []});
+      newList.save();
+      res.redirect("/" + customList);
     }
     else{
-        const userinput = req.body.sometext;
-        todoitems.push(userinput);
-        res.redirect("/");
+      res.render("list", {kindOfDay: foundone.name, itemarrays: foundone.items});
     }
-    
+  })
+  .catch((err)=>{
+    console.log(err);
+  }
+
+  )
+
 })
 
 app.listen("3000",function(){
